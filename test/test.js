b566298aaa29c12bd981cd0cb7503febf11a3467
Kelsie Belan <kelsie70@gmail.com>
2018-11-13 16:26:10 -0500

    added soql queries

--- test/test.js
+++ test/test.js
@@ -1,16 +1,68 @@
 // My API key:
 // 2cK1XYfoKd3Fl3ZYysflTtbDA
 
+
+//https://data.seattle.gov/resource/tjb6-zsmc.json?$select=materialtype,count(checkouts)&$group=materialtype
+//https://data.seattle.gov/resource/tjb6-zsmc.json?$select=title,count(checkouts),avg(checkouts)&$group=title&$where= (checkouts > 400)AND(checkoutyear=2016)
+//top 10 book checkouts for a month and year
+//https://data.seattle.gov/resource/tjb6-zsmc.json?$select=title&$order=checkouts DESC&$where=(checkoutyear=2014)AND(checkoutmonth=10)AND(materialtype='BOOK')
+//https://data.seattle.gov/resource/tjb6-zsmc.json?$select=title,checkoutmonth,checkouts&$order=checkouts DESC&$where=(checkoutyear=2014)AND(materialtype='BOOK')
+
 $.ajax({
-    url: "https://data.seattle.gov/resource/5src-czff.json?itemtype=acdvd",
+<<<<<<< HEAD
+    url: "https://data.seattle.gov/resource/tjb6-zsmc.json?$select=title,checkoutmonth,checkouts&$order=checkouts DESC&$where=(checkoutyear=2014)AND(materialtype='BOOK')",
+=======
+    url: "https://data.seattle.gov/resource/5src-czff.json?checkoutyear=2018",
+>>>>>>> 2c03ac4609e2a960f44a7fe2c189dc46ff8551e9
     type: "GET",
     data: {
-      "$limit" : 5000,
-      "$$app_token" : "2cK1XYfoKd3Fl3ZYysflTtbDA"
+      "$limit" : 120,
+      "$$app_token" : "gj5klMMMFIV45YA3S1Qkk8Ssd"
     }
 }).done(function(data) {
   alert("Retrieved " + data.length + " records from the dataset!");
-  console.log(data);
+
+  //experiment
+  var subjectsList = [];
+  data.forEach(function(checkout) {
+    if (checkout.subjects != null) {
+      var temp = checkout.subjects.split(',');
+      temp.forEach(function(subject) {
+        // console.log("This is subjects list now", subjectsList);
+        // if (subjectsList[subject] != null) {
+        //   subjectsList[subject] += 1;
+        // } else {
+        //   subjectsList[subject] = 1;
+        // }
+        if(subject != null) {
+          subjectsList.push(subject);
+        }
+      });
+    }
+  });
+
+  var frequencyCounts = {};
+  var dictionary = [];
+  for (var i = 0; i < subjectsList.length; i++) {
+    var num = subjectsList[i];
+    frequencyCounts[num] = (frequencyCounts[num] || 0) + 1;
+  }
+
+  for (var i = 0; i < Object.keys(frequencyCounts).length; i++) {
+    var key = Object.keys(frequencyCounts)[i];
+    var value = Object.values(frequencyCounts)[i];
+    dictionary.push({"subject": key, "frequency": value })
+  }
+
+  dictionary.sort(function(a, b) {
+    return b.frequency - a.frequency;
+  });
+
+  console.log(dictionary);
+
+
+
+  // console.log(subjectsList.sort());
 
   var books = d3.select('#book-titles');
 
@@ -23,6 +75,7 @@ $.ajax({
   //         return book['itemtitle'] + '. ' + book['collection'] + ' about ' + book['subjects'];
   //     });
 
+    /*
     var bkTableBody = d3.select('#book-table tbody');
 
     var trBook = bkTableBody.selectAll('tr')
@@ -33,18 +86,20 @@ $.ajax({
     trBook.append('td')
         .style('text-align','center')
         .text(function(book){
-            return book['itemtitle'];
+            return book['title'];
         });
 
     trBook.append('td')
         .text(function(book){
-            return book['collection'];
+            return book['materialtype'];
         });
 
     trBook.append('td')
         .style('text-align','center')
         .text(function(book){
-            return book['subjects'];
+            return book['checkouts'];
         });
+
+    */
 
 });
