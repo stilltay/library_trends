d034e05ec7a7fa42108e5933bd521ab817f2a8dc
Taylor Stillman <tstillman3@gatech.edu>
2018-12-06 12:36:07 -0500

    bar chart updating now

--- creator/creator.js
+++ creator/creator.js
@@ -1,48 +1,63 @@
+// Get layout parameters
+var svgWidthC = 1000;
+var svgHeightC = 500;
 
+var paddingC = {t: 0, r: 40, b: 30, l: 200};
+
+// Compute chart dimensions
+var chartWidthC = svgWidthC - paddingC.l - paddingC.r;
+var chartHeightC = svgHeightC - paddingC.t - paddingC.b;
+
+ var svgC = d3.select('#creator').append("svg")
+    .attr("width", svgWidthC + paddingC.l+paddingC.r)
+.attr("height", svgHeightC+ paddingC.t+paddingC.b);
+
+// Compute the spacing for bar bands based on all 26 letters
+var barBand = chartHeightC / 12;
+var barHeight = barBand * 0.7;
+
+// Create a group element for appending chart elements
+var chartGC = svgC.append('g')
+.attr('transform', 'translate('+[paddingC.l, paddingC.t]+')');
+
+svgC.append('text')
+  .attr('class', 'chartTitle')
+  .attr('id', 'chartTitle')
+  .attr('transform', 'translate(200, 20)')
+  .style("fill", "white")
+  .style("font-size", "18px")
+  .text('Top 10 Checkout Items for ');
+
+var xScale = d3.scaleLinear()
+      .domain([0, 10])
+      .range([0,500]);
+
+var xAxis = d3.axisBottom(xScale)
+
+svgC.append('g')
+  .attr('id', 'x-axis')
+  .attr('class', 'x-axis')
+  .attr('transform', 'translate(200,450)')
+  .attr('stroke', 'white')
+  .call(xAxis);
 
 function updateCreatorChart(type, name) {
-    
-    // Get layout parameters
-    var svgWidth = 1000;
-    var svgHeight = 500;
 
-    var padding = {t: 0, r: 40, b: 30, l: 200};
-
-    // Compute chart dimensions
-    var chartWidth = svgWidth - padding.l - padding.r;
-    var chartHeight = svgHeight - padding.t - padding.b;
-    
-     var svgC = d3.select('#creator').append("svg")
-        .attr("width", svgWidth + padding.l+padding.r)
-		.attr("height", svgHeight+ padding.t+padding.b);
-
-    // Compute the spacing for bar bands based on all 26 letters
-    var barBand = chartHeight / 12;
-    var barHeight = barBand * 0.7;
-
-    // Create a group element for appending chart elements
-    var chartG = svgC.append('g')
-    .attr('transform', 'translate('+[padding.l, padding.t]+')');
-    
-        svgC.append('text')
-    	.attr('class', 'chartTitle')
-        .attr('transform', 'translate(200, 20)')
-        .style("fill", "white")
-        .style("font-size", "18px")
+    svgC.select('#chartTitle')
     	.text('Top 10 Checkout Items for ' + name);
 
 
 d3.csv('../scatterplot_begins/library.csv', function(error, dataset) {
 
   if (error) throw error;
-    
+
     var filteredData;
-    
+
     if(type == "creator"){
         filteredData = dataset.filter(function(d) {
            if(d.creator == name) {
                return d;
-           } 
+           }
         });
     } else if(type == "publisher"){
         filteredData = dataset.filter(function(d) {
@@ -51,11 +66,11 @@ d3.csv('../scatterplot_begins/library.csv', function(error, dataset) {
             }
         });
     }
-    
+
 
     console.log("filtered data");
     console.log(filteredData);
-    
+
     var nestedFilteredData = d3.nest()
         .key(function(d) {
             return d.title;
@@ -65,66 +80,63 @@ d3.csv('../scatterplot_begins/library.csv', function(error, dataset) {
             });
         })
         .entries(filteredData);
-    
+
     console.log("nested filtered data");
     console.log(nestedFilteredData);
-    
+
     var newData=[];
     console.log(nestedFilteredData[0].key);
-        
+
     for(var i =0 ; i < nestedFilteredData.length; i++){
-        
+
         newData.push({
             title: nestedFilteredData[i].key,
             total_checkouts: nestedFilteredData[i].value
         });
-    
+
     }
-    
-   
+
+
     newData.sort(function(a,b) {
         return b.total_checkouts-a.total_checkouts;
     });
-    
+
     if(newData.length>9){
         newData = newData.slice(0,10);
     }
-    
+
      console.log("newData");
         console.log(newData);
-    
-    
+
+
     // **** Draw and Update your chart here ****
-    
+
     freqExtent = d3.extent(newData, function(d){
 		return parseFloat(d.total_checkouts);
 	   });
-    
-    var freqMax = d3.max(newData, function(d){
+
+    freqMax = d3.max(newData, function(d){
         return d.total_checkouts;
     });
-    
+
     console.log("freq");
     console.log(freqMax);
-    
-    var xScale = d3.scaleLinear()
+
+    xScale = d3.scaleLinear()
         .domain([0, freqMax])
         .range([0,500]);
-    
-        var xAxis = d3.axisBottom(xScale).ticks(5);
-    
-        svgC.append('g')
-        .attr('class', 'x-axis')
-        .attr('transform', 'translate(200,450)')
-        .attr('stroke', 'white')
-        .call(xAxis);
-        
+
+    xAxis = d3.axisBottom(xScale);
+
+        svgC.select('#x-axis')
+          .call(xAxis);
+
     /*
         letterExtent = d3.extent(newData, function(d){
 		return d.title;
 	       });
     */
-    
+
         //console.log("letter extent", letterExtent);
 
     /*
@@ -132,23 +144,23 @@ d3.csv('../scatterplot_begins/library.csv', function(error, dataset) {
         .domain([0, 5])
         .rangeRound([40,260])
         .padding(0.5);*/
-    
-    var bars = chartG.selectAll('.bar')
+
+    var bars = chartGC.selectAll('.bar')
         .data(newData, function(d){
             return d.title;
         });
-    
+
     var barsEnter = bars.enter()
         .append('g')
         .attr('class', 'bar');
-        
+
 	bars.merge(barsEnter)
 		.attr('transform', function(d,i){
             return 'translate('+[0, i * barBand + 50]+')';
         });
-    
+
     console.log("This is barheight", barHeight);
-    
+
     barsEnter.append('rect')
         .attr('height', barHeight)
         .attr('width', function(d){
@@ -167,12 +179,10 @@ d3.csv('../scatterplot_begins/library.csv', function(error, dataset) {
              var tx = d.title.substring(0, d.title.indexOf("/"));
             return tx;
         })
-        .attr('fill', 'white');   
-    
-        bars.exit().remove();
-    
-});
-    
-}
+        .attr('fill', 'white');
 
-                
+        bars.exit().remove();
+
+});
+
+}
